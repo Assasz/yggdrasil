83b5c4914639d6b4e6bdcfeaf089b8e0ef5d82a0
Assasz <pawel.assasz678@gmail.com>
2018-02-27 01:57:42 +0100

    Add methods declarations for ConfigurationInterface

--- src/Yggdrasil/Core/Configuration/ConfigurationInterface.php
+++ src/Yggdrasil/Core/Configuration/ConfigurationInterface.php
@@ -2,15 +2,49 @@
 
 namespace Yggdrasil\Core\Configuration;
 
+use Yggdrasil\Core\Driver\Base\DriverInstanceCollection;
+use Yggdrasil\Core\Exception\DriverNotFoundException;
+
 /**
  * Interface ConfigurationInterface
  *
- * Provides connection between application configuration and framework core drivers
+ * Provides connection between application configuration and framework core
  *
  * @package Yggdrasil\Core\Configuration
  * @author Paweł Antosiak <contact@pawelantosiak.com>
  */
 interface ConfigurationInterface
 {
+    /**
+     * Loads registered drivers and returns collection of their instances
+     *
+     * @return DriverInstanceCollection
+     */
+    public function loadDrivers(): DriverInstanceCollection;
 
+    /**
+     * Loads given driver and returns it's instance directly
+     *
+     * @param string $key Name of driver
+     * @return mixed
+     *
+     * @throws DriverNotFoundException if given driver doesn't exist
+     */
+    public function loadDriver(string $key);
+
+    /**
+     * Returns configuration
+     *
+     * @return array
+     */
+    public function getConfiguration(): array;
+
+    /**
+     * Checks if given data exist in configuration
+     *
+     * @param array  $keys    Set of keys of data to check
+     * @param string $section Name of configuration file section, in which given keys should exist
+     * @return bool
+     */
+    public function isConfigured(array $keys, string $section): bool;
 }
