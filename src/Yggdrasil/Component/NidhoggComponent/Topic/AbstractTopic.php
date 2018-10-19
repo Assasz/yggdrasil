0343dcfed2ad7eeeadca8d491bfa962f25f92753
Assasz <pawel.assasz678@gmail.com>
2018-10-19 18:35:31 +0200

    [Nidhogg] Add missing methods to AbstractTopic

--- src/Yggdrasil/Component/NidhoggComponent/Topic/AbstractTopic.php
+++ src/Yggdrasil/Component/NidhoggComponent/Topic/AbstractTopic.php
@@ -99,6 +99,41 @@ abstract class AbstractTopic implements TopicInterface, WampServerInterface
     }
 
     /**
+     * When a new connection is opened it will be passed to this method
+     *
+     * @param  ConnectionInterface $conn The socket/connection that just connected to your application
+     * @throws \Exception
+     */
+    public function onOpen(ConnectionInterface $conn): void
+    {
+
+    }
+
+    /**
+     * This is called before or after a socket is closed (depends on how it's closed).  SendMessage to $conn will not result in an error if it has already been closed.
+     *
+     * @param  ConnectionInterface $conn The socket/connection that is closing/closed
+     * @throws \Exception
+     */
+    public function onClose(ConnectionInterface $conn): void
+    {
+
+    }
+
+    /**
+     * If there is an error with one of the sockets, or somewhere in the application where an Exception is thrown,
+     * the Exception is sent back down the stack, handled by the Server and bubbled back up the application through this method
+     *
+     * @param  ConnectionInterface $conn
+     * @param  \Exception          $e
+     * @throws \Exception
+     */
+    public function onError(ConnectionInterface $conn, \Exception $e): void
+    {
+
+    }
+
+    /**
      * Returns allowed origins for topic
      *
      * @return array
